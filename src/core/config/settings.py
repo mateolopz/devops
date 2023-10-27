66245d06924d7ac6e478580dc262e50f768edaaf
veronica <v.escobar1@uniandes.edu.co>
2023-10-27 15:14:26 -0500

    (fix) Changed settings.py for local tests while developing testing out pytest for unit tests.

--- src/core/config/settings.py
+++ src/core/config/settings.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    DB_NAME: str = os.getenv("RDS_DB_NAME")
-    DB_USER: str = os.getenv("RDS_USERNAME")
-    DB_PASSWORD: str = os.getenv("RDS_PASSWORD")
-    DB_HOST: str = os.getenv("RDS_HOSTNAME")
-    DB_PORT: str = os.getenv("RDS_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
 
 class AuthSettings(BaseSettings):
     authjwt_secret_key:str = os.getenv('AUTHJWT_SECRET_KEY')
